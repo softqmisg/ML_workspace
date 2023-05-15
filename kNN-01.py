d46d98e9b0ddea23064bf41cc4eed4f556814cd1
Mehdi Bagherimofidi <mehdi.bagherimofidi@yahoo.com>
2023-05-15 12:45:31 +0330

    add k-NN tutorial from https://realpython.com/knn-python/

---
+++ kNN-01.py
@@ -0,0 +1,12 @@
+import sys
+print(sys.version)
+print (sys.version_info)
+
+# import pandas as pd
+
+# url = (
+#     "https://archive.ics.uci.edu/ml/machine-learning-databases"
+#     "/abalone/abalone.data"
+# )
+# abalone = pd.read_csv(url, header=None)
+# abalone.head()
